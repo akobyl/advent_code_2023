d5dbc5ac6bfc4b7aa347e2e8caf5bdc81361b004
Andy Kobyljanec <kobyl125@gmail.com>
2024-04-18 20:23:09 -0400

    Updated template to only use single input.txt file

--- daily-template/src/bin/part1.rs
+++ daily-template/src/bin/part1.rs
@@ -3,7 +3,7 @@ use {{crate_name}}::part1::process;
 
 #[tracing::instrument]
 fn main() {
-    let file = include_str!("../../input1.txt");
+    let file = include_str!("../../input.txt");
 
 
     let result = process(file);
--- daily-template/src/bin/part2.rs
+++ daily-template/src/bin/part2.rs
@@ -3,7 +3,7 @@ use {{crate_name}}::part2::process;
 
 #[tracing::instrument]
 fn main() {
-    let file = include_str!("../../input2.txt");
+    let file = include_str!("../../input.txt");
 
 
     let result = process(file);
